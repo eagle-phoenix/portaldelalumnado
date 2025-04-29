ea397038f6cacb5674928ef6438d5b014ee0dde0
Fran <41526932+eagle-phoenix@users.noreply.github.com>
2025-04-29 21:55:25 +0200

    Update scripts.js
    limpieza de comentarios

--- scripts.js
+++ scripts.js
@@ -3,12 +3,7 @@ const urls = {
     expediente: "https://proxy-alumnado-production-41d7.up.railway.app/datos?tipo=expediente"
 };
 
-/*
-function fetchSheetData(url) {
-    return fetch(url)
-        .then(res => res.json());
-}
-*/
+
 function fetchSheetData(url) {
     return fetch(url)
         .then(res => res.json());
